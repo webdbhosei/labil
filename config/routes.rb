57c2c93b9046c5df21b45603eb2b6d1b47bf3cb1
asam <asam@glaxy.rails>
2016-05-26 20:01:57 +0900

    create devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "topviews#index"
+
+  match ':controller(/:action(/:id))', via:[:get, :post, :patch]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
