4b5a8af94d2ec7c385863a7e435402f7a98e9948
C-Lotte <nine_ball_256@yahoo.co.jp>
2016-06-16 13:20:48 +0900

    destroy message model and scaffold message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,2 @@
 class Message < ActiveRecord::Base
-  belongs_to :member
-  
-#  validates :member_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608113733) do
+ActiveRecord::Schema.define(version: 20160615010653) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
