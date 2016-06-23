6f3643680027f2a09ceeb3d3494d16d2f1e12401
C-Lotte <nine_ball_256@yahoo.co.jp>
2016-06-23 14:51:34 +0900

    if you use messages, you must create 3 members or more

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ActiveRecord::Base
   has_one :user
+  has_many :messages
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,9 @@ Location.create( name: 'zaishitsu')
 Location.create( name: 'gaishutsu')
 Location.create( name: 'kitaku')
 
+Message.create( member_id: 1, message: "test_1")
+Message.create( member_id: 2, message: "test_2")
+Message.create( member_id: 1, message: "test_1snd")
+Message.create( member_id: 3, message: "test_3")
+
+
