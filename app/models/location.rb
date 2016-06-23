2ac4769c23100016a165b6c49551969a78559823
Object Oriented in Ruby <takuma.sasaki.3h@stu.hosei.ac.jp>
2016-06-23 13:59:58 +0900

    write some codes of status table

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ActiveRecord::Base
+  has_many :statuses 
+  has_many :members, through: :statuses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Location.create( name: 'zaishitsu')
 Location.create( name: 'gaishutsu')
 Location.create( name: 'kitaku')
 
+ActiveRecord::Base.transaction do
+location = Location.last
+member = Member.new(name: "test")
+member.save!
+member = Member.find_by_name("test")
+status = Status.new(member_id: member.id, location_id: location.id)
+status.save!
+end
