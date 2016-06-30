dbbf6af48932d0efe7cff47ee8f75c90e9892a30
C-Lotte <nine_ball_256@yahoo.co.jp>
2016-06-30 15:28:53 +0900

    modify message's error and add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,17 +22,27 @@ Location.create( :name=> '藤田研', :category=> '2')
 
 ActiveRecord::Base.transaction do
 location = Location.last
-member = Member.new(name: "test", email: "test@test.com", password:"0000", password_confirmation: "0000")
+member = Member.new(name: "Suzuki", email: "test@test.com", password:"0000", password_confirmation: "0000")
 member.save!
-member = Member.find_by_name("test")
+member1 = Member.new(name: "Tanaka", email: "test1@test.com", password:"0000", password_confirmation: "0000")
+member1.save!
+member2 = Member.new(name: "Takahashi", email: "test2@test.com", password:"0000", password_confirmation: "0000")
+member2.save!
+member = Member.find(1)
 status = Status.new(member_id: member.id, location_id: location.id)
 status.save!
+member = Member.find(2)
+status1 = Status.new(member_id: member.id, location_id: location.id)
+status1.save!
+member = Member.find(3)
+status2 = Status.new(member_id: member.id, location_id: location.id)
+status2.save!
 end
 
-Message.create( member_id: 1, message: "test_1")
-Message.create( member_id: 2, message: "test_2")
-Message.create( member_id: 1, message: "test_1snd")
-Message.create( member_id: 3, message: "test_3")
+Message.create( member_id: 1, message: "Hello Everyone!")
+Message.create( member_id: 2, message: "This is test sentence")
+Message.create( member_id: 1, message: "I wanna go home...")
+Message.create( member_id: 3, message: "ypaaaaaaaaaaa!!")
 
 Schedule.create(:member_id => 1, :event_start => 'Mon, 30 May 2016 09:30:00', :event_end => 'Mon, 30 May 2016 11:00:00', :venue => 'A-1')
 Schedule.create(:member_id => 1, :event_start => 'Mon, 30 May 2016 13:30:00', :event_end => 'Mon, 30 May 2016 15:00:00', :venue => 'W101')
